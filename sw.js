6be1c660dd7bbef3d5a014a3426d667127f900a5
Patrick Roberts <git@patrickroberts.dev>
2023-11-23 18:02:29 -0600

    Use dynamic cache instead of static cache

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-oninstall=e=>e.waitUntil(caches.open('v1').then(c=>c.addAll(['/','/favicon-16x16.png','/favicon-32x32.png','/favicon.ico','/icon.png'])));onfetch=e=>e.respondWith(caches.match(e.request))
+oninstall=e=>e.waitUntil(caches.open('v1').then(c=>onfetch=(e,q)=>e.respondWith(c.match(q=e.request).then(m=>m??fetch(q)).then(s=>s.ok?c.put(q,s.clone()).then(()=>s):s))))
