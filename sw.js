d655407f03922ca1ce565cbf5fe330b003980a95
Patrick Roberts <git@patrickroberts.dev>
2025-02-25 21:52:27 -0600

    Update sw.js
    synchronously listen to fetch

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-oninstall=e=>e.waitUntil(caches.open('v1').then(c=>onfetch=(e,q)=>e.respondWith(c.match(q=e.request).then(m=>m??fetch(q)).then(s=>s.ok?c.put(q,s.clone()).then(()=>s):s))))
+onfetch=(e,q)=>e.respondWith(caches.open('v1').then(c=>c.match(q=e.request).then(m=>m??fetch(q)).then(s=>s.ok?c.put(q,s.clone()).then(()=>s):s)))
